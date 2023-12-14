ff2e1d65a2d697d221f5abb49c40f0a8520f0398
Jens Arvidsson <jensarv@gmail.com>
2023-12-14 08:36:06 +0100

    progress day 13 part 2

--- src/day_13.rs
+++ src/day_13.rs
@@ -21,15 +21,47 @@ pub fn solve() {
 
   let mut total = 0;
   for p in patterns {
+
     let vertical = vertical_reflection(&p, false);
+    let mut gottem = false;
     if vertical.is_some() {
       //print_matrix_vertical(&p, vertical.unwrap());
-      total += vertical.unwrap();
+
+      //generate all others, try them return the one that gives value
+      let new_one = generate_new_matrices(&p);
+      for n in new_one {
+        let vert = vertical_reflection(&n, false);
+        let hori = horizontal_reflection(&n, false);
+        if vert.is_some() && vert.unwrap() != vertical.unwrap() {
+          total += vert.unwrap();
+          gottem = true;
+          break;
+        } else if hori.is_some() && hori.unwrap() != vertical.unwrap() {
+          total += 100 * hori.unwrap();
+          gottem = true;
+          break;
+        }
+      }
     } else {
       let horizontal = horizontal_reflection(&p, false);
       if horizontal.is_some() {
+        let new_one = generate_new_matrices(&p);
+        for n in new_one {
+          let vert = vertical_reflection(&n, false);
+          let hori = horizontal_reflection(&n, false);
+          if vert.is_some() && vert.unwrap() != horizontal.unwrap() {
+            total += vert.unwrap();
+            
+            gottem = true;
+            break;
+          } else if hori.is_some() && hori.unwrap() != horizontal.unwrap() {
+            total += 100 * hori.unwrap();
+            gottem = true;
+            break;
+          }
+        }
         //print_matrix_horizontal(&p, horizontal.unwrap());
-        total += 100 * horizontal.unwrap()
+        //total += 100 * horizontal.unwrap()
       } else {
         print_matrix(&p);
         println!("               ");
@@ -39,22 +71,79 @@ pub fn solve() {
         println!("--------------")
       }
     }
+    if !gottem {
+      print_matrix(&p);
+      println!("               ");
+      vertical_reflection(&p, true);
+      println!("~~~~~~~~~~~~~");
+      horizontal_reflection(&p, true);
+      println!("--------------")
+    }
+
   }
   println!("total: {}", total);
 
 }
 
-fn vertical_reflection(p: &Vec<String>, print:bool) -> Option<usize> {
-  //spin it 90 deg and call horiz
+fn generate_new_matrices(p: &Vec<String>) ->  Vec<Vec<String>> {
+  let mut ret = vec![];
+  for r in 0..p.len() {
+    let row = &p[r];
+    for (i,c) in row.chars().enumerate() {
+      let mut new_row = row.clone().chars().collect::<Vec<char>>();
+      new_row[i] = convert(c);
+      let mut new_matrix = p.clone();
+      new_matrix[r] = new_row.iter().collect::<String>();
+      ret.push(new_matrix);
+    }
+  }
+  return ret;
+}
+
+fn generate_new_matrices_horizontal(p: &Vec<String>, before: usize) -> Option<usize> {
+  println!("``````````");
+  print_matrix(&p);
+  println!("``````````");
+  for r in 0..p.len() {
+    let row = &p[r];
+    println!("rooooooooooooooooooow");
+    println!("befor: {}", row);
+    for (i,c) in row.chars().enumerate() {
+      let mut new_row = row.clone().chars().collect::<Vec<char>>();
+      
+      new_row[i] = convert(c);
+      println!("after: {}", new_row.iter().collect::<String>());
+      let mut new_matrix = p.clone();
+      new_matrix[r] = new_row.iter().collect::<String>();
+      let horizontal = horizontal_reflection(&new_matrix, false);
+      if horizontal.is_some() && horizontal.unwrap() != before {
+        return horizontal;
+      }
+    }
+    println!("eeeeeeennnnnnnnddddddd");
+  }
+  return None;
+}
+
+fn convert(s:char) -> char {
+  return if s == '#' { '.' } else { '#' }
+}
+
+fn rotate_matrix(p: &Vec<String>) -> Vec<String> {
   let mut new_vec = vec![];
   for i in 0..p[0].len() {
     let mut new_row = vec![];
     for row in p {
       new_row.push(row.chars().nth(i).unwrap());
     }
-    new_vec.push(new_row.iter().collect::<String>())
+    new_vec.push(new_row.iter().collect::<String>());
   }
-    return horizontal_reflection(&new_vec, print);
+  return new_vec;
+}
+
+fn vertical_reflection(p: &Vec<String>, print:bool) -> Option<usize> {
+  let rotated = rotate_matrix(&p);
+    return horizontal_reflection(&rotated, print);
 }
 
 fn horizontal_reflection(p: &Vec<String>, print:bool) -> Option<usize> {
