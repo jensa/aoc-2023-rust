24445f5b273caae9be9c5727e3ba6de347fd4b58
Jens Arvidsson <jensarv@gmail.com>
2023-12-18 09:03:04 +0100

    day 14 part 2

--- src/day_14.rs
+++ src/day_14.rs
@@ -1,3 +1,5 @@
+use std::collections::HashMap;
+
 use crate::util::{self};
 
 pub fn solve() {
@@ -14,6 +16,64 @@ pub fn solve() {
   //mutate the matrix to roll the rocks, start from the top:
   // first row - nothing can happen
   // second row - move eligble up until it hits a rock or 0
+  let mut cache = HashMap::new();
+  //keep track of our rotation;
+  let mut i = 0;
+  let factor = 1000000000-1;
+  let mut found = false;
+  let mut found_cycle = "".to_string();
+  let mut found_cycle_at = 0;
+  let mut cycle_len = 1;
+
+  //rotate so it starts right (we start with a roll)
+  map = rotate_matrix_90_right(&map);
+  map = rotate_matrix_90_right(&map);
+  map = rotate_matrix_90_right(&map);
+  while i < factor {
+    map = cycle(map);
+    let cache_key = matrix_key(&map);
+    if cache_key == found_cycle && cycle_len == 1 {
+      cycle_len = i - found_cycle_at;
+      let left = factor - i;
+      let rest = left % cycle_len;
+      let to_add = left - rest;
+      i += to_add -1 ;
+    }
+    if !found && cache.contains_key(&cache_key) {
+      found_cycle = cache_key.to_string();
+      found_cycle_at = i;
+      found = true;
+    }
+    cache.insert(cache_key, map.clone());
+    i += 1;
+    
+  }
+  map = rotate_matrix_90_right(&map); //since we start with a rot
+  let mut total_load = 0;
+  for y in 0..map.len() {
+    for x in 0..map[y].len() {
+      if map[y][x] == 'O' {
+        total_load += map.len()-y;
+      }
+    }
+  }
+
+  print_matrix(&map);
+  println!("total_load: {}", total_load);
+
+}
+
+fn cycle(map: Vec<Vec<char>>) -> Vec<Vec<char>> {
+  let mut new_map = map.clone();
+  for i in 0..4 {
+    new_map = rotate_matrix_90_right(&new_map);
+    roll_north(&mut new_map);
+  }
+  return new_map;
+}
+
+
+fn roll_north(map: &mut Vec<Vec<char>>) {
 
   for y in 0..map.len() {
     for x in 0..map[y].len() {
@@ -28,19 +88,28 @@ pub fn solve() {
       }
     }
   }
+}
 
-  let mut total_load = 0;
-  for y in 0..map.len() {
-    for x in 0..map[y].len() {
-      if map[y][x] == 'O' {
-        total_load += map.len()-y;
-      }
+fn rotate_matrix_90_right(p: &Vec<Vec<char>>) -> Vec<Vec<char>> {
+  let mut new_vec = vec![];
+  for x in 0..p[0].len() { //for x
+    let mut new_row: Vec<char> = vec![];
+    for y in (0..p.len()).rev() { // for y
+      let row = &p[y];
+      new_row.push(*row.iter().nth(x).unwrap());
     }
+    new_vec.push(new_row);
+  }
+  return new_vec;
+}
+
+fn matrix_key(p: &Vec<Vec<char>>) -> String {
+  let mut r = vec![];
+  for v in p {
+    r.push(v.iter().collect::<String>());
   }
 
-  print_matrix(&map);
-  println!("total_load: {}", total_load);
-
+  return r.join(" ");
 }
 
 fn north_is_empty(y:usize, x:usize, map:&Vec<Vec<char>>) -> bool {
